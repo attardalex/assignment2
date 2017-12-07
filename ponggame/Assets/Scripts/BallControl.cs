4dc620dfb13a7710188d0692b23e22f8e1ae53cc
Alexander Attard <a100950@mcast.org.mt>
2017-12-07 11:41:13 +0100

    Startbutton - load level1 paddle1movement ballmovement

---
+++ ponggame/Assets/Scripts/BallControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    //choose random direction (lef/right), ball will then move
+    void MoveBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand  < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("MoveBall", 2);//will give 2 seconds waiting time for player to prepare, then MoveBall function will start
+	}
+
+    //resets ball to center, when goal is scored and from one level to another
+    void Resetball(){
+        vel = Vector2.zero;
+        rb2d.velocity = vel;
+        transform.position = Vector2.zero;
+    }
+
+    //waits for ball to hit paddle, velocity of ball is adjusted alongside the speed of the paddle
+   void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("paddle1"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+    void Update () {
+		
+	}
+}
---
+++ ponggame/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    //code that will determine the keys
+    //for paddles1 movement and also the speed of the paddle
+
+    public KeyCode moveup = KeyCode.W;
+    public KeyCode movedown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f; //highest position for paddle to go 
+    private Rigidbody2D rb2d;
+ 
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    //tells us what button is pressed, what movement (up/down)
+    //if no button is pressed it will remain still
+    void Update () {
+
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveup))
+        {
+            vel.y = speed;
+        }
+        else if (Input.GetKey(movedown))
+        {
+            vel.y = -speed;
+        }
+        else if (!Input.anyKey)
+        {
+            vel.y = 0;
+        }
+        rb2d.velocity = vel;
+
+        var pos = transform.position;
+        if(pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+	}
+}
